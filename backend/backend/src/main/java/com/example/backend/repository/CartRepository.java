459a6634128ddf47857a1f818fc1c865b25abe26
Surya Arunesh <124980732+suryaarun2831@users.noreply.github.com>
2024-11-12 22:06:20 +0530

    Delete backend/backend directory

--- backend/backend/src/main/java/com/example/backend/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-// package com.example.backend.repository;
-
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// import com.example.backend.model.Cart;
-
-// public interface CartRepository extends JpaRepository<Cart, Long> {
-// }
--- backend/backend/src/main/java/com/example/backend/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.backend.model.User;
-
-public interface UserRepository extends MongoRepository<User,Integer> {
-    
-}
